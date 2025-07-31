d46b85ffaffb6a2281ad0a6a639ba173bb171cdd
peregr1ne <61361793+peregr1ne@users.noreply.github.com>
2025-07-31 16:11:53 +0200

    add: JS generator working

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function generateStory(event) {
   event.preventDefault();
 
-  new Typewriter("#story", {
-    strings: "Story of the character is told here",
-    autoStart: true,
-    delay: 10,
+  let charInput = document.querySelector("#request-input");
+  let char = charInput.value;
+
+  let apiKey = `tb68bfo76465b10730ef185037e7aaa0`;
+  let context =
+    "You are one of the Vampire the masquerade game designers and one of the best story tellers, please provide detailed and interesting answer";
+  let prompt = `please generate a breathtaking ${char}'s background story, which aligns well with VTM v5 lore. Separate each paragraph with <br>.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(({ data: { answer } }) => {
+    new Typewriter("#story", {
+      strings: answer,
+      autoStart: true,
+      delay: 15,
+    });
   });
 }
 
